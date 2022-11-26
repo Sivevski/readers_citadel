35f23eb0c742babfec177dda4b0620bd23d1fd5a
sivevski <sivevski@gmail.com>
2022-11-26 14:11:26 +0100

    Changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_26_112059) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -23,9 +26,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_26_112059) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.text "body"
-    t.integer "book_id", null: false
+    t.bigint "book_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
